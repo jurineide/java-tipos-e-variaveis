8a6a323f82b347e6bacd6c754f3e4527cff776b5
jurineide <jurineidesouza@gmail.com>
2023-08-21 09:31:27 -0300

    tipos variaveis

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,19 @@
+package src;
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        byte idade=3;
+        short mes=12;
+        String cep= "05728020"; // porque começa com zero
+        String cpf="00000000"; // stryng pq pode começar com zero
+        float pi=3.14F;
+        double salario= 2500.55;
+        long numeroEstrelas=12345678910L; // Long precisa terminar com um L.
+        int ano= 2023;
+
+        final String NOME= "Jurineide"; // use o final e o nome da stryng de letra maiuscula para garantir que ela não seja alterada
+        
+        System.out.println(NOME);
+
+    }
+    
+}
